754592e4ff845371bda8b4889c7d6ac3b685a9ef
Moebiuss8 <162041778+Moebiuss8@users.noreply.github.com>
2024-12-30 11:52:18 +0100

    Add patients page component

---
+++ frontend/app/patients/page.tsx
@@ -0,0 +1,9 @@
+import PatientManagement from '@/components/PatientManagement';
+
+export default function PatientsPage() {
+  return (
+    <main className="container mx-auto py-8">
+      <PatientManagement />
+    </main>
+  );
+}
